c88b544d73def1239ab4d037dc19885041ea0de1
johnsouz <johnatan.souza@estudante.ufjf.br>
2023-04-14 20:28:08 -0300

    Renomeações

--- T1/meshGenerators.js
+++ T1/meshGenerators.js
@@ -1,55 +1,45 @@
 import * as THREE from 'three';
-import { createGroundPlaneWired } from '../libs/util/util.js';
+import { createGroundPlaneWired, setDefaultMaterial } from '../libs/util/util.js';
 
 export function createAirplane() {
     const geometry = new THREE.CylinderGeometry(2, 1.2, 25, 32);
-    let material1 = new THREE.MeshPhysicalMaterial({
-        color: "yellow",
-    })
-    const tronco = new THREE.Mesh(geometry, material1);
+    let yellowMaterial = setDefaultMaterial("yellow");
+    const tronco = new THREE.Mesh(geometry, yellowMaterial);
     tronco.rotateX(Math.PI / 2);
 
     // criando ponta traseira
     const geometry6 = new THREE.SphereGeometry(1.2, 32, 16);
-    const ponta = new THREE.Mesh(geometry6, material1);
+    const ponta = new THREE.Mesh(geometry6, yellowMaterial);
     ponta.position.set(0, -12.5, 0);
     ponta.scale.set(1, 1.3, 1);
     tronco.add(ponta);
 
     // criando estabilizadores
-    let material = new THREE.MeshPhysicalMaterial({
-        color: "red",
-        transparent: true,
-    })
-    const estabH = new THREE.Mesh(geometry6, material);
+    let redMaterial = setDefaultMaterial("red");
+    const estabH = new THREE.Mesh(geometry6, redMaterial);
     estabH.position.set(0, -12.5, 0);
     estabH.scale.set(6, 1, .1);
     tronco.add(estabH);
 
-    const estabV = new THREE.Mesh(geometry6, material);
+    const estabV = new THREE.Mesh(geometry6, redMaterial);
     estabV.position.set(0, -12.5, -1.6);
     estabV.scale.set(.2, 1, 3);
     tronco.add(estabV);
 
     // criando o visor
     const geometry2 = new THREE.SphereGeometry(1, 32, 16);
-    let material2 = new THREE.MeshPhysicalMaterial({
-        color: "blue",
-        transparent: true,
-        opacity: 0.7,
-    })
-    const visor = new THREE.Mesh(geometry2, material2);
+    let blueMaterial = setDefaultMaterial("blue");
+    blueMaterial.transparent = true;
+    blueMaterial.opacity = 0.7;
+
+    const visor = new THREE.Mesh(geometry2, blueMaterial);
     visor.scale.set(1, 2.4, 1);
     visor.position.set(0, 3, -1.6);
     tronco.add(visor);
 
     // criando as asas
     const geometry3 = new THREE.CylinderGeometry(2, 2, 30, 32);
-    let material3 = new THREE.MeshPhysicalMaterial({
-        color: "red",
-        transparent: true,
-    })
-    const cylinder = new THREE.Mesh(geometry3, material3);
+    const cylinder = new THREE.Mesh(geometry3, redMaterial);
     cylinder.rotateZ(Math.PI / 2);
     cylinder.position.set(0, 4, 0);
     cylinder.scale.set(1, 1, 0.4);
@@ -57,7 +47,7 @@ export function createAirplane() {
 
     // criando a haste da hélice
     const geometry4 = new THREE.CylinderGeometry(.2, .2, 3, 32);
-    const cylinder2 = new THREE.Mesh(geometry4, material);
+    const cylinder2 = new THREE.Mesh(geometry4, redMaterial);
     cylinder2.rotateY(Math.PI / 2);
     cylinder2.position.set(0, 12.5, 0);
     tronco.add(cylinder2);
@@ -65,18 +55,15 @@ export function createAirplane() {
 
     // criando a hélice
     const geometry5 = new THREE.CylinderGeometry(2.2, 2.2, .4, 32);
-    let material6 = new THREE.MeshPhysicalMaterial({
-        color: "white",
-        transparent: true,
-        opacity: 0.5,
-        side: THREE.DoubleSide
-    })
-    const cylinder3 = new THREE.Mesh(geometry5, material6);
+    let whiteMaterial = setDefaultMaterial("white");
+    whiteMaterial.transparent = true;
+    whiteMaterial.opacity = 0.5;
+    whiteMaterial.side = THREE.DoubleSide;
+
+    const cylinder3 = new THREE.Mesh(geometry5, whiteMaterial);
     cylinder3.rotateY(Math.PI / 2);
     cylinder3.position.set(0, 13.75, 0);
     tronco.add(cylinder3);
-
-    // tronco.add(new THREE.AxesHelper(10));
 
     let holder = new THREE.Object3D();
     holder.add(tronco.rotateZ(Math.PI));
@@ -86,28 +73,25 @@ export function createAirplane() {
 
 export function createTree() {
     const geometry = new THREE.CylinderGeometry(2, 2, 20, 32);
-    let material1 = new THREE.MeshPhysicalMaterial({
-        color: "chocolate",
-        transparent: true,
-    })
-    const madeira = new THREE.Mesh(geometry, material1);
+    let brownMaterial = setDefaultMaterial("chocolate");
+    brownMaterial.transparent = true;
+    const madeira = new THREE.Mesh(geometry, brownMaterial);
 
     const geometry6 = new THREE.SphereGeometry(4, 32, 16);
-    let material2 = new THREE.MeshPhysicalMaterial({
-        color: "green",
-        transparent: true,
-    })
-    const folhas = new THREE.Mesh(geometry6, material2);
+    let greenMaterial = setDefaultMaterial("green");
+    greenMaterial.transparent = true;
+
+    const folhas = new THREE.Mesh(geometry6, greenMaterial);
     folhas.position.set(0, 8, 0);
     folhas.scale.set(.8, .8, .8);
     madeira.add(folhas);
 
-    const folhas2 = new THREE.Mesh(geometry6, material2);
+    const folhas2 = new THREE.Mesh(geometry6, greenMaterial);
     folhas2.position.set(0, 5, -2);
     folhas2.scale.set(.9, .9, .9);
     madeira.add(folhas2);
 
-    const folhas3 = new THREE.Mesh(geometry6, material2);
+    const folhas3 = new THREE.Mesh(geometry6, greenMaterial);
     folhas3.position.set(-1, 4, 2);
     madeira.add(folhas3);
 
